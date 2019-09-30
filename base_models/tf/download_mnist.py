79a4458e0e19dcf495127fb48472be36b0775718
Taylor Childers <taylor@jtchilders.com>
2019-09-30 16:58:42 +0000

    removing previous mnist/hvd/tf model and adding new one

--- base_models/tf/download_mnist.py
+++ base_models/tf/download_mnist.py
@@ -3,7 +3,7 @@ Download MNIST data using Keras interface
 """
 from keras.datasets import mnist
 import os,keras
-download_path = os.getcwd() + '/minst.tgz'
+download_path = os.getcwd() + '/mnist.tgz'
 
 print(download_path)
 (x_train, y_train), (x_test, y_test) = mnist.load_data(download_path)
